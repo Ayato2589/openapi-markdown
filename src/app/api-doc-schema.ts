ecec07b6fad7147e3e178238c8e6c49ae7a3c948
Ayato2589 <ayatoa2589@gmail.com>
2025-06-21 23:21:04 +0900

    ApiDocSchemaの構造を変更

--- src/app/api-doc-schema.ts
+++ src/app/api-doc-schema.ts
@@ -1,13 +1,42 @@
 import { OpenAPI } from "openapi-types";
 
+// type Parameter = {
+//     name: string;
+//     type: 'int' | 'string' | 'date';
+//     required?: boolean;
+//     minLength?: number;
+//     maxLength?: number;
+//     regExpPattern?: string;
+//     description?: string;
+//     example?: string;
+// };
+
+// type Model = {
+//     children: Model[];
+// };
+
+// type Response = {
+//     httpStatusCode: number;
+//     body?: Model;   
+// }
+
+type Endpoint = {
+    path: string;
+    // httpMethod: 'GET' | 'PUT' | 'PATCH' | 'DELETE';
+    // pathParameters: Parameter[];
+    // queryParameters: Parameter[];
+    // requestBody?: Model
+    // response: Response[]
+};
+
 export type ApiDocSchema = {
-    endpoints: string[];
+    endpoints: Endpoint[];
 };
 
 export function generateApiDocSchema(apiDoc: OpenAPI.Document): ApiDocSchema {
-    const endpoints = Object.keys(apiDoc.paths || {});
+    const endpoints: Endpoint[] = Object.keys(apiDoc.paths || {}).map((path) => ({
+        path,
+    }));
 
-    return {
-        endpoints: endpoints,
-    }
+    return { endpoints };
 }
